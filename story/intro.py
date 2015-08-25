2a68ea291f7e55d3ffaa8c65454554bddac67354
Patrick Mays <Blueroofgames@gmail.com>
2015-08-24 20:50:50 -0400

    save

--- story/intro.py
+++ story/intro.py
@@ -1,2 +1,6 @@
-"""welcome to the relm of yinefell. you are a professinal explorer who has been chosen
-to go to the ancient kingdom of detlasale.
+"""
+
+welcome to the relm of yinefell. you are a professinal explorer who has been chosen
+to go to the ancient kingdom of detlasale. you may run into a few beasts on your way, but
+there will be a reward. Are you sure you want to continue?
+"""
