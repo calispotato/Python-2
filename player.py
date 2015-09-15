e20b29b51b0aa588d42e7a619721ac5ff3a28251
Patrick Mays <Blueroofgames@gmail.com>
2015-09-14 20:29:15 -0400

    save

---
+++ player.py
@@ -0,0 +1,5 @@
+"""Player Class"""
+
+from stats import player
+
+class Player()
---
+++ stats.py
@@ -0,0 +1,8 @@
+"""HasStats Interface"""
+
+class HasStats():
+    strength = 0
+    courage = 0
+    health = 0
+    wisdom = 0
+
